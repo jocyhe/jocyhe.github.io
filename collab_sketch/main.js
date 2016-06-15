f79f4aad79f7b8e923052f32a09e75df277dfe66
Jocy He <jocyhe@gmail.com>
2016-06-15 22:27:20 +0000

    yay

--- collab_sketch/main.js
+++ collab_sketch/main.js
@@ -1,3 +1,5 @@
+ var r_g_b; 
+ 
  var config = {
     apiKey: "AIzaSyDN8WQQUiR1cd-ypbsjrNIL3vUpti16LPc",
     authDomain: "collab-sketch-21480.firebaseapp.com",
@@ -13,7 +15,16 @@
  function setup() {
     var canvas = createCanvas($(document).width(),$(document).height()-39);
     background(255);
-    fill(0);
+    ColorPicker(
+                    document.getElementById('slide'),
+                    document.getElementById('picker'),
+                    function(hex, hsv, rgb) {
+                    r_g_b = hex;
+                    })
+    
+    fill(212, 31, 2);
+    
+    strokeWeight(0);
     pointsData.on("child_added", function (point) {
         points.push(point.val()); 
     });
@@ -27,15 +38,17 @@
 
  function draw() { 
     background(255);
-    
+    // document.getElementById("jscolor").style.backgroundColor.replace("rgb","fill");
+
     for (var i = 0; i < points.length; i++) {
         var point = points[i];
+        fill(point.color);
         ellipse(point.x,point.y, 5, 5);
     }
  }
  
  function drawPoint() {
-    pointsData.push({x: mouseX, y: mouseY});
+    pointsData.push({x: mouseX, y: mouseY, color: r_g_b});
  }
  
  $("#saveDrawing").on("click", saveDrawing);
@@ -50,8 +63,6 @@
      pointsData.remove();
      points = [];
  }
- 
- 
  
  
  
